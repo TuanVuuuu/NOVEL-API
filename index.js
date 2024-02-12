0471fef5662be05e4e7ba29894e18486c07f2f26
Nguyen Tuan Vu <vunt@vnpt-technology.vn>
2024-02-12 20:03:35 +0700

    UPDATE SERVER URL

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ app.use(cors());
 
 app.use(morgan("common"));
 dotenv.config();
-const urlBase = 'https://truyenfull.vn/';
 
 (async () => {
   const browser = await puppeteer.launch();
@@ -26,31 +25,28 @@ const urlBase = 'https://truyenfull.vn/';
 
 app.get('/v1/:novel/page-:page', async (req, res) => {
   try {
-      const browser = await puppeteer.launch({ headless: true }); // Mở trình duyệt ở chế độ ẩn
-      const page = await browser.newPage();
-      await page.goto(urlBase + req.params.novel + '/trang-' + req.params.page + '/#list-chapter');
-      const novelInfo = await page.evaluate(() => {
-
-
-          const chapters = Array.from(document.querySelectorAll('.list-chapter li')).map(li => {
-              const linkElement = li.querySelector('a');
-              const chapterTitle = linkElement.getAttribute('title');
-              const chapterUrl = linkElement.getAttribute('href');
-              return {
-                  title: linkElement.textContent.trim(),
-                  url: 'http://localhost:8000/v1' + chapterUrl.split('https://truyenfull.vn')[1]
-              };
-          });
-          return {
-              chapters: chapters
-          };
+    const browser = await puppeteer.launch({ headless: true }); // Mở trình duyệt ở chế độ ẩn
+    const page = await browser.newPage();
+    await page.goto(process.env.BASE_URL + '/' + req.params.novel + '/trang-' + req.params.page + '/#list-chapter');
+    const novelInfo = await page.evaluate((baseUrl) => {
+      const chapters = Array.from(document.querySelectorAll('.list-chapter li')).map(li => {
+        const linkElement = li.querySelector('a');
+        const chapterUrl = linkElement.getAttribute('href');
+        return {
+          title: linkElement.textContent.trim(),
+          url: baseUrl + '/v1' + chapterUrl.split(baseUrl)[1]
+        };
       });
+      return {
+        chapters: chapters
+      };
+    }, process.env.BASE_URL);
 
-      await browser.close();
-      res.status(200).json(novelInfo);
+    await browser.close();
+    res.status(200).json(novelInfo);
   } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: error.name });
+    console.error(error);
+    res.status(500).json({ error: error.name });
   }
 });
 

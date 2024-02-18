ed1f8c7e97e722c0a266299728cde5ca31fbcc7c
Nguyen Tuan Vu <vunt@vnpt-technology.vn>
2024-02-18 11:21:19 +0700

    UPDATE API GET NOVEL INFO

--- controllers/novel_controller.js
+++ controllers/novel_controller.js
@@ -25,7 +25,7 @@ const novelController = {
             // Chờ cho ít nhất một phần tử chứa danh sách chương xuất hiện trên trang web
             await page.waitForSelector('.media-body', { timeout: 320000 });
 
-            // await autoScroll(page)
+            await autoScroll(page)
 
             // Trích xuất thông tin về tiểu thuyết
             const listDataTeam = await page.evaluate(() => {
